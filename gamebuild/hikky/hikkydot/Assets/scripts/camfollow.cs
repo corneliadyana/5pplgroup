a9941064ccdc6116eba95fccfcdf04e22e409775
trashynucleus <norikita@windowslive.com>
2017-10-25 23:12:33 +0700

    bEtTer BuIld
    basically lvl 1 almost done. What's left : trigger senter, trigger
    keluar, dialogue und sub, ganti komputer pas mati lampu, overlay warna
    gelap pas mati lampu, masukkin song sama sfx sigh

---
+++ gamebuild/hikky/hikkydot/Assets/scripts/camfollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camfollow : MonoBehaviour {
+	public float speedo = 0.1f;
+	public Transform farleft;
+	public Transform farright;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved) {
+			Vector2 touchDeltaPosition = Input.GetTouch (0).deltaPosition;
+			transform.Translate (-touchDeltaPosition.x * Time.deltaTime * speedo, 0, 0);
+
+			if (transform.position.x > farright.position.x)
+				transform.position = new Vector3 (farright.position.x, transform.position.y, transform.position.z);
+
+			if (transform.position.x < farleft.position.x)
+				transform.position = new Vector3 (farleft.position.x, transform.position.y, transform.position.z);
+		}
+	}
+}
+
+// camera restrict movement
