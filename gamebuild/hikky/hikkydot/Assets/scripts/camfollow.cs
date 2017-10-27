e23da7402be31b7461ea92f365f7fd7aa6c0bf74
trashynucleus <norikita@windowslive.com>
2017-10-27 21:57:56 +0700

    flamldasmd

--- gamebuild/hikky/hikkydot/Assets/scripts/camfollow.cs
+++ gamebuild/hikky/hikkydot/Assets/scripts/camfollow.cs
@@ -3,18 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class camfollow : MonoBehaviour {
-	public float speedo = 0.1f;
-	public Transform farleft;
-	public Transform farright;
+	public Transform playerToFollow;
+	public float Height;
+	public float Zoom;
+	public float max;
+	public float min;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved) {
+		if (playerToFollow.position.x > min && playerToFollow.position.x < max) {
+			transform.position = new Vector3 (playerToFollow.position.x, Height, Zoom);
+		
+		}
+	}
+}
+
+// camera restrict movement
+
+/*	public float speedo = 0.1f;
+	public Transform farleft;
+	public Transform farright;
+ * 
+ * if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved) {
 			Vector2 touchDeltaPosition = Input.GetTouch (0).deltaPosition;
 			transform.Translate (-touchDeltaPosition.x * Time.deltaTime * speedo, 0, 0);
 
@@ -23,8 +38,4 @@ public class camfollow : MonoBehaviour {
 
 			if (transform.position.x < farleft.position.x)
 				transform.position = new Vector3 (farleft.position.x, transform.position.y, transform.position.z);
-		}
-	}
-}
-
-// camera restrict movement
+		}*/
